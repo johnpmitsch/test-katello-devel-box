5ee06a6bc42629d893d2e30e57f25723b1707ccd
John Mitsch <jomitsch@redhat.com>
2018-05-19 20:38:24 -0400

    Use FileUtils

--- run.rb
+++ run.rb
@@ -1,29 +1,40 @@
 #!/usr/bin/ruby
+require "fileutils"
 
 # Script to show current katello developer happiness with gifs
 
 box_name = "centos7-devel"
+command = "vagrant up #{box_name}"
 forklift_location="/home/jomitsch/sat-deploy/forklift"
-dir_location="/var/www/html/"
-webpage = File.join(dir_location, "index.html")
+boxes_location = File.join(forklift_location, "vagrant/boxes.d/")
+apache_location="/var/www/html/"
+webpage_location = File.join(apache_location, "index.html")
+script_location = "/home/jomitsch/test-katello-devel-box"
 current_date = Time.now
+
+FileUtils.rm("/tmp/console.out")
 
 Dir.chdir(forklift_location) do |dir|
   `git fetch --all`
   `git reset --hard origin/master`
-  `cp vagrant/boxes.d/99-local.yaml.example vagrant/boxes.d/99-local.yaml`
+   FileUtils.cp(File.join(boxes_location, "99-local.yaml.example"), 
+		File.join(boxes_location, "99-local.yaml"))
   `vagrant destroy -f #{box_name}`
-  `vagrant up #{box_name} &> #{dir_location}/console.out`
+  `#{command} &> /tmp/console.out`
 end
 
 if $?.success?
-  `cp -f success.html #{webpage}`
+  puts "success"
+  FileUtils.cp(File.join(script_location, "success.html"), webpage_location)
 else
-  `cp -f failure.html #{webpage}`
+  puts "failure"
+  FileUtils.cp(File.join(script_location, "failure.html"), webpage_location)
 end
+
+FileUtils.cp("/tmp/console.out", File.join(apache_location, "console.out"))
 
 Dir.chdir(forklift_location) do |dir|
   `vagrant destroy -f #{box_name}`
 end
 
-`echo \"Ran on #{current_date}\" >> #{webpage}`
+`echo \"Ran on #{current_date}\" >> #{webpage_location}`
